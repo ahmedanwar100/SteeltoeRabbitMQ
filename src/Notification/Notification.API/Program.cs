ca17e82c6e363dae5d1b5ac188e8d40ff282b8ca
ahmed0304 <ahmedanwer657@gmail.com>
2022-07-12 23:51:13 +0500

    steeltoe rabbitmq setup

--- src/Notification/Notification.API/Program.cs
+++ src/Notification/Notification.API/Program.cs
@@ -1,9 +1,37 @@
+using Common.SeedWork;
+using Notification.API;
+using Steeltoe.Connector.RabbitMQ;
+using Steeltoe.Messaging.RabbitMQ.Config;
+using Steeltoe.Messaging.RabbitMQ.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+#region Services
+// Add singleton that will process incoming messages
+builder.Services.AddSingleton<ListenerService>();
+#endregion
+
+#region SteelToeRabbitMQ Configuration
+// Add Steeltoe Rabbit services, use JSON serialization
+builder.Services.AddRabbitServices(true);
+
+// Add Steeltoe RabbitAdmin services to get queues declared
+builder.Services.AddRabbitAdmin();
+
+// Add Steeltoe RabbitTemplate for sending/receiving
+builder.Services.AddRabbitTemplate();
+
+// Add a queue to the message container that the rabbit admin will discover and declare at startup
+builder.Services.AddRabbitQueue(new Queue(Queues.ProductAddQueue));
+
+// Tell steeltoe about singleton so it can wire up queues with methods to process queues
+builder.Services.AddRabbitListeners<ListenerService>();
+#endregion
 
 var app = builder.Build();
 
@@ -16,28 +44,4 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-       new WeatherForecast
-       (
-           DateTime.Now.AddDays(index),
-           Random.Shared.Next(-20, 55),
-           summaries[Random.Shared.Next(summaries.Length)]
-       ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
 app.Run();
-
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
--- src/Product/Product.API/Program.cs
+++ src/Product/Product.API/Program.cs
@@ -1,9 +1,39 @@
-var builder = WebApplication.CreateBuilder(args);
+using Common.SeedWork;
+using Product.API.Models.Requests;
+using Steeltoe.Connector.RabbitMQ;
+using Steeltoe.Messaging.RabbitMQ.Config;
+using Steeltoe.Messaging.RabbitMQ.Core;
+using Steeltoe.Messaging.RabbitMQ.Extensions;
 
+var builder = WebApplication.CreateBuilder(args);
+// Build a config object, using env vars and JSON providers.
+IConfiguration config = new ConfigurationBuilder()
+    .AddJsonFile($"appsettings.Development.json")
+    .AddEnvironmentVariables()
+    .Build();
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+#region SteelToeRabbitMQ Configuration
+//when you call ConfigureRabbitOptions method then appsettings.env.json specified connection configuration will be applied otherwise
+//if you dont call that method then default settings will be picked and even you dont need to specify any configuration in appsettings
+builder.Services.ConfigureRabbitOptions(config);
+// Add Steeltoe Rabbit services, use JSON serialization
+builder.Services.AddRabbitServices(true);
+
+// Add Steeltoe RabbitAdmin services to get queues declared
+builder.Services.AddRabbitAdmin();
+
+// Add Steeltoe RabbitTemplate for sending/receiving
+builder.Services.AddRabbitTemplate();
+
+// Add a queue to the message container that the rabbit admin will discover and declare at startup
+builder.Services.AddRabbitQueue(new Queue(Queues.ProductAddQueue));
+#endregion
+
 
 var app = builder.Build();
 
@@ -16,28 +46,14 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 
-app.MapGet("/weatherforecast", () =>
+app.MapPost("/product", (AddProductRequestDTO request, RabbitTemplate _rabbitTemplate, RabbitAdmin _rabbitAdmin) =>
 {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-       new WeatherForecast
-       (
-           DateTime.Now.AddDays(index),
-           Random.Shared.Next(-20, 55),
-           summaries[Random.Shared.Next(summaries.Length)]
-       ))
-        .ToArray();
-    return forecast;
+    var msg = new Message() {Type="Information", Body = "Hi there from over here." };
+
+    _rabbitTemplate.ConvertAndSend(Queues.ProductAddQueue, msg);
+
+    return Results.Ok("Product Added");
 })
 .WithName("GetWeatherForecast");
-
 app.Run();
-
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
